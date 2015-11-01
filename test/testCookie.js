537c55bd44f6af1d5f94085066f875c7709ea77a
Tim <timevko@gmail.com>
2015-11-01 18:58:07 -0500

    testing complete

--- test/testCookie.js
+++ test/testCookie.js
@@ -1,11 +1,10 @@
 
 describe('CRUDCookie', function() {
-
     it('Should exist', function() {
 		_smplCke.set('test', {'foo' : 'bar', 1 : true});
 		expect(_smplCke.is('test')).toBe(true);
 		expect(typeof _smplCke.get('test')).toBe('object');
 		_smplCke.del('test');
-		expect(_smplCke.is('test')).toBe(true);
+		expect(_smplCke.is('test')).toBe(false);
     });
 });
