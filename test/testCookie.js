4cfcbb0cc7b160d9c0c2cd9f3ffd2435122c5667
Tim <timevko@gmail.com>
2015-11-01 18:47:40 -0500

    testing

--- test/testCookie.js
+++ test/testCookie.js
@@ -1,13 +1,11 @@
 
 describe('CRUDCookie', function() {
-	_smplCke.set('test', {'foo' : 'bar', 1 : true});
-	console.log(document.cookie);
+
     it('Should exist', function() {
-        expect(_smplCke.is('test')).toBe(true);
-        expect(typeof _smplCke.get('test')).toBe('object');
-    });
-    _smplCke.del('test');
-    it('Should not exist', function() {
-        expect(typeof _smplCke.is('test')).toBe(false);
+		_smplCke.set('test', {'foo' : 'bar', 1 : true});
+		expect(_smplCke.is('test')).toBe(true);
+		expect(typeof _smplCke.get('test')).toBe('object');
+		_smplCke.del('test');
+		expect(typeof _smplCke.is('test')).toBe(false);
     });
 });
